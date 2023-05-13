6ee1b071616c449c2264695e653f63d8c21b7485
Abrar2165 <108695905+Abrar2165@users.noreply.github.com>
2023-05-13 10:07:26 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,111 +1,123 @@
-var hole_obj="";
-var ball_obj="";
+
 var canvas = new fabric.Canvas('myCanvas');
-
-ball_y = 0;
-ball_x = 0;
-hole_y = 400;
-hole_x = 800;
-
-
+ball_y=0;
+ball_x=0;
+hole_y=400;
+hole_x=800;
 
 block_image_width = 5;
 block_image_height = 5;
 
-function load_img() {
-	fabric.Image.fromURL("golf-h1.png", function (Img) {
+function load_img(){
+	fabric.Image.fromURL("golf-h.png", function(Img) {
 		hole_obj = Img;
-		hole_obj.scaletoWidth(50);
-		hole_obj.scaletoHeight(50);
+		hole_obj.scaleToWidth(50);
+		hole_obj.scaleToHeight(50);
 		hole_obj.set({
-			top: hole_y,
-			left: hole_x
+			top:hole_y,
+			left:hole_x
 		});
 		canvas.add(hole_obj);
-	});
+		});
 	new_image();
 }
 
-function new_image() {
-	fabric.Image.fromURL("ball.png", function (Img) {
-		ball_obj = Img;
-		ball_obj.scaletoWidth(50);
-		ball_obj.scaletoHeight(50);
-		ball_obj.set({
-			top: ball_y,
-			left: ball_x
-		});
-		canvas.add(ball_obj);
+function new_image()
+{
+	fabric.Image.fromURL("ball.png", function(Img) {
+	ball_obj = Img;
+	ball_obj.scaleToWidth(50);
+	ball_obj.scaleToHeight(50);
+	ball_obj.set({
+	top:ball_y,
+	left:ball_x
+	});
+	canvas.add(ball_obj);
 	});
 }
 
 window.addEventListener("keydown", my_keydown);
 
-function my_keydown(e) {
+function my_keydown(e)
+{
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
-	if ((ball_x == hole_x) && (ball_y == hole_y)) {
+	if((ball_x==hole_x)&&(ball_y==hole_y)){
 		canvas.remove(ball_obj);
-		document.getElementById("hd3").innerHTML = "YOU HIT THE GOAL!!!";
-		document.getElementById("myCanvas").style.borderColor = "red";
-	} else {
-		if (keyPressed == '38') {
+		console.log("You have Hit the Goal!!!");
+		document.getElementById("hd3").innerHTML="You have Hit the Goal!!!";
+	    document.getElementById("myCanvas").style.borderColor="red";
+	}
+	else{
+		if(keyPressed == '38')
+		{
 			up();
 			console.log("up");
 		}
-		if (keyPressed == '40') {
+		if(keyPressed == '40')
+		{
 			down();
 			console.log("down");
 		}
-		if (keyPressed == '37') {
+		if(keyPressed == '37')
+		{
 			left();
 			console.log("left");
 		}
-		if (keyPressed == '39') {
+		if(keyPressed == '39')
+		{
 			right();
 			console.log("right");
 		}
 	}
-
-	function up() {
-		if (ball_y >= 0) {
+	
+	function up()
+	{
+		if(ball_y >=5)
+		{
 			ball_y = ball_y - block_image_height;
 			console.log("block image height = " + block_image_height);
-			console.log("When Up arrow key is pressed, X = " + ball_x + " , Y = " + ball_y);
+			console.log("When Up arrow key is pressed, X =  " + ball_x + " , Y = "+ball_y);
 			canvas.remove(ball_obj);
 			new_image();
 		}
-
 	}
 
-	function down() {
-		if (ball_y <= 450) {
-			ball_y = ball_y - block_image_height;
+	function down()
+	{
+		if(ball_y <=450)
+		{
+			ball_y = ball_y + block_image_height;
 			console.log("block image height = " + block_image_height);
-			console.log("When Down arrow key is pressed, X = " + ball_x + " , Y = " + ball_y);
+			console.log("When Down arrow key is pressed, X =  " + ball_x + " , Y = "+ball_y);
 			canvas.remove(ball_obj);
 			new_image();
 		}
 	}
 
-	function left() {
-		if (ball_x > 5) {
+	function left()
+	{
+		if(ball_x >5)
+		{
 			ball_x = ball_x - block_image_width;
 			console.log("block image width = " + block_image_width);
-			console.log("When Left arrow key is pressed, X = " + ball_x + " , Y = " + ball_y);
+			console.log("When Left arrow key is pressed, X =  " + ball_x + " , Y = "+ball_y);
 			canvas.remove(ball_obj);
 			new_image();
 		}
 	}
 
-	function right() {
-		if (ball_x <= 1050) {
-			ball_x = ball_x - block_image_width;
+	function right()
+	{
+		if(ball_x <=1050)
+		{
+			ball_x = ball_x + block_image_width;
 			console.log("block image width = " + block_image_width);
-			console.log("When Right arrow key is pressed, X = " + ball_x + " , Y = " + ball_y);
+			console.log("When Right arrow key is pressed, X =  " + ball_x + " , Y = "+ball_y);
 			canvas.remove(ball_obj);
 			new_image();
 		}
 	}
-
+	
 }
+
